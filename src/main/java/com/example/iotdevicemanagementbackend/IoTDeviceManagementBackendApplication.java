abdc8745f7b417e40f0d640a9b206e87e2f387d8
BaicaiCoin <3210102367@zju.edu.cn>
2023-11-16 23:24:55 +0800

    v0.0

---
+++ src/main/java/com/example/iotdevicemanagementbackend/IoTDeviceManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.iotdevicemanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTDeviceManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoTDeviceManagementBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iotdevicemanagementbackend/IoTDeviceManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iotdevicemanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTDeviceManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
